c7e9ba95ddccce2f7c7c3381d9dee62724c087f8
Nevan Scott <nfscott@gmail.com>
2017-11-16 16:08:09 -0800

    initial fractal v2 setup

---
+++ fractal.config.js
@@ -0,0 +1,29 @@
+
+module.exports = {
+
+  app: {
+    src: './components',
+    engines:[
+      '@frctl/fractal-engine-nunjucks'
+    ],
+    plugins:[]
+  },
+
+  pages: {
+    src: './styleguide',
+    dest: './build',
+    site: {
+      title: 'Example Styleguide',
+    },
+    routes: {
+      components: {
+        collection: 'library.components',
+        template: '_component.njk',
+        permalink: 'components/{component.id}'
+      }
+    },
+  },
+
+  inspector: {}
+
+};
