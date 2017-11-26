59c77e3f4a74664a0d38b94849949ede4fec3f9d
Nevan Scott <nfscott@gmail.com>
2017-11-27 00:41:10 +0100

    Variants and Scenarios for Banner component

--- components/@banner/config.js
+++ components/@banner/config.js
@@ -2,7 +2,57 @@ module.exports = {
 
   variants: [
     {
-      id: 'default'
+      id: 'default',
+      scenarios: [
+        {
+          label: 'Nevan Scott',
+          context: {
+            siteName: 'Nevan Scott',
+            navLinks: [
+              {
+                url: '#',
+                text: 'Writing'
+              },
+              {
+                url: '#',
+                text: 'Portfolio'
+              },
+              {
+                url: '#',
+                text: 'Teaching'
+              },
+              {
+                url: '#',
+                text: 'Reading'
+              }
+            ]
+          }
+        },
+        {
+          label: 'Notated',
+          context: {
+            siteName: 'notated.org',
+            navLinks: [
+              {
+                url: '#',
+                text: 'Blog'
+              },
+              {
+                url: '#',
+                text: 'About'
+              },
+              {
+                url: '#',
+                text: 'Projects'
+              },
+              {
+                url: '#',
+                text: 'Archive'
+              }
+            ]
+          }
+        }
+      ]
     }
   ]
 
