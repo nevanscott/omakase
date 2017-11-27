0e8dcf4595d30b0de9dfc0093e5164a6f2943e93
Nevan Scott <nfscott@gmail.com>
2017-11-27 20:07:01 +0100

    Adds link component

---
+++ components/@link/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  variants: [
+    {
+      id: 'default',
+      scenarios: [
+        {
+          label: 'Default',
+          context: {
+            url: '#',
+            text: 'Example Link'
+          }
+
+        }
+      ]
+    }
+  ]
+
+};
